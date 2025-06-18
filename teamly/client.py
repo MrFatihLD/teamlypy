4c4dbbc288c9e1b26ab6d89041eab2c3def91a0e
Fatih Kuloglu <fatihkuloglu@proton.me>
2025-06-18 18:13:37 +0200

    removed teamly/client.py codeblocks

--- teamly/client.py
+++ teamly/client.py
@@ -9,28 +9,19 @@ CoroT = TypeVar('CoroT', bound=Callable[..., Coro[Any]])
 
 class Client:
 
-    #initializing
     def __init__(self):
-        self.http = HTTPClient()
-        self.tasks: List[asyncio.Task] = []
-        self._userInfo: Dict[Any] = {}
-        self._teamInfo: Dict[Any] = {}
+        pass
 
-    #HTTPClient ile oturum acar
     async def start(self,token):
         await self.connect(token)
 
-    #Client'i calisiracak fonksiyon
     def run(self,token: str):
-        print(f"{__name__}: got token (token: {token})") # Log
-
+        
         async def runner():
             async with self:
-                print(f"{__name__}: starting") # Log
                 await self.start(token)
 
         try:
-            print(f"{__name__}: running \"runner()\" function in asyncio") # Log
             asyncio.run(runner())
         except KeyboardInterrupt:
             pass
@@ -43,31 +34,10 @@ class Client:
 
 
     async def connect(self, token: str):
-
-        await self.http.connect()
-
-        #bu while loop bizim Client'in kapatana kadar hep acik kalmasini saglar
-        while True:
-            print("Client is running") # Log
-            await asyncio.sleep(5)
+        pass
 
     async def close(self):
-        print(f"\n{__name__}: calling the function http.close()") # Log
-
-        await self.http.close()
-
-        await self.cancel_tasks()
-    
-    async def add_task(self, task):
-        task = asyncio.create_task(task)
-        self.tasks.append(task)
-        return task
-    
-    async def cancel_tasks(self):
-        for task in self.tasks:
-            task.cancel()
-
-        await asyncio.gather(*self.tasks, return_exceptions=True)
+        pass
 
 
     
