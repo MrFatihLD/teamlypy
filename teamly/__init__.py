1a074dcca996eec2ee4c2e0e9b0ac126f1f0ef89
Fatih Kuloglu <fatihkuloglu@proton.me>
2025-06-18 18:06:48 +0200

    modified teamly/__init__.py, http.py & utils.py

--- teamly/__init__.py
+++ teamly/__init__.py
@@ -1,3 +1,4 @@
 from .client import *
 from .http import *
 from .gateway import *
+from .utils import *
--- teamly/http.py
+++ teamly/http.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import yarl
+from teamly.utils import MISSING
 
 from typing import ClassVar, Optional
 
@@ -17,15 +18,34 @@ class Route:
 
 class HTTPClient:
     
-    def __init__(self):
-        self.__session: aiohttp.ClientSession = None
-        self.connection: Optional[aiohttp.BaseConnector] = None
+    def __init__(
+            self,
+            loop: asyncio.AbstractEventLoop = None, # temporaly None
+            connector: Optional[aiohttp.BaseConnector] = None,
+            *,
+            proxy: Optional[str] = None,
+            proxy_auth: Optional[aiohttp.BasicAuth] = None,
+            #unsync_clock: bool = True,
+            http_trace: Optional[aiohttp.TraceConfig] = None,
+            #max_ratelimit_timeout: Optional[float] = None
+        ):
+        self.loop: asyncio.AbstractEventLoop = loop
+        self.connector: Optional[aiohttp.BaseConnector] = connector or MISSING
+        self.proxy: Optional[str] = proxy
+        self.proxy_auth: Optional[aiohttp.BasicAuth] = proxy_auth
+        self.http_trace: Optional[aiohttp.TraceConfig] = http_trace
+
+        self.__session: aiohttp.ClientSession = MISSING
+
+
         self.token: str = None
         self.ws_url: str = "https://api.teamly.one/api/v1"
 
     #function to connect to WebSocket
     async def ws_connect(self, url: str) -> aiohttp.ClientWebSocketResponse:
         kwargs = {
+            "proxy": self.proxy,
+            "proxy_auth": self.proxy_auth,
             "timeout": 30,
             "autoclose": False,
             "headers": {
@@ -36,7 +56,13 @@ class HTTPClient:
     
     #function to login to ClientSession
     async def static_login(self, token: str):
-        pass
+        if self.connector is MISSING:
+            self.connector = aiohttp.TCPConnector(limit=0)
+
+        self.__session = aiohttp.ClientSession(
+            connector = self.connector,
+            trace_configs = None if self.http_trace is None else self.http_trace
+        )
     
     #function will close the session
     async def close(self):
